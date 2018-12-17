e316c77577a6a10da53c9be42f970aec854cd077
John Cordero <johncorderox@gmail.com>
2018-12-17 01:36:05 -0800

    new controller queries

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,9 @@ class HomeController < ApplicationController
 
   def index
     @user    = current_user
+    @topics  = Topic.joins(:user)
+               .select("topics.id AS topics_id, topics.title AS topics_title, topics.subject AS topics_subject, topics.content AS topics_content, users.email AS users_email")
+               .order(created_at: :desc)
+               .limit(10)
   end
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -6,11 +6,24 @@ class TopicsController < ApplicationController
   end
 
   def new
+    @user = current_user
   end
 
   def create
+    @new_topic = Topic.new(new_topic)
+    if @new_topic.save
+      redirect_to home_path
+    else
+      redirect_to :back
+    end
   end
 
   def destroy
   end
+
+  private
+
+    def new_topic
+      params.require(:topic).permit(:title, :subject, :content, :user_id)
+    end
 end
