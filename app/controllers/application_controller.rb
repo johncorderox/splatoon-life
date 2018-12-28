ce8193ac0729cf8fc342be595bde678a089db071
John Cordero <johncorderox@gmail.com>
2018-12-28 14:55:41 -0800

    new model for users and devo notes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_up_path_for(resource)
     home_path
   end
 
   def after_sign_in_path_for(resource)
     home_path
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :current_password, :location])
   end
 
 
